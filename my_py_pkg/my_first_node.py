de9b3d5d57eee40358111e5e1c6d99f436214634
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-02 13:40:53 +0800

    wrote oop

--- my_py_pkg/my_first_node.py
+++ my_py_pkg/my_first_node.py
@@ -2,11 +2,22 @@
 import rclpy
 from rclpy.node import Node
 
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__("py_test")
+        self.counter_ = 0
+        self.get_logger().info("Hello ROS2")
+        self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        self.counter_ += 1
+        self.get_logger().info("Hello " + str(self.counter_))
+
 
 def main(args=None):
     rclpy.init(args=args)
-    node = Node("py_test")
-    node.get_logger().info("Hello ROS2")
+    node = MyNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
