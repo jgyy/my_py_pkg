86ec1870411b0be3eb7d64d64b4ba054a4f838e7
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-03 22:10:01 +0800

    created service client with oop

--- my_py_pkg/add_two_ints_client.py
+++ my_py_pkg/add_two_ints_client.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-
+from functools import partial
 from example_interfaces.srv import AddTwoInts
 
 
-class AddTwoIntsServerNode(Node):
+class AddTwoIntsClientNode(Node):
     def __init__(self):
-        super().__init__("add_two_ints_server")
-        self.server_ = self.create_service(
-            AddTwoInts, "add_two_ints", self.callback_add_two_ints)
-        self.get_logger().info("Add two ints server had been started.")
+        super().__init__("add_two_ints_client")
+        self.call_add_two_ints_server(6, 7)
+        self.call_add_two_ints_server(3, 1)
+        self.call_add_two_ints_server(5, 2)
 
-    def callback_add_two_ints(self, request, response):
-        response.sum = request.a + request.b
-        self.get_logger().info(str(request.a) + " + " + str(request.b)
-                               + " = " + str(response.sum))
-        return response
+    def call_add_two_ints_server(self, a, b):
+        client = self.create_client(AddTwoInts, "add_two_ints")
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("Waiting for Server Add Two Ints...")
+
+        request = AddTwoInts.Request()
+        request.a = a
+        request.b = b
+
+        future = client.call_async(request)
+        future.add_done_callback(partial(self.callback_call_add_two_ints, a=a, b=b))
+
+    def callback_call_add_two_ints(self, future, a, b):
+        try:
+            response = future.result()
+            self.get_logger().info(str(a) + " + " + str(b)
+                                   + " = " + str(response.sum))
+
+        except Exception as e:
+            self.get_logger().error("Service call failed %r" % (e,))
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = AddTwoIntsServerNode()
+    node = AddTwoIntsClientNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
