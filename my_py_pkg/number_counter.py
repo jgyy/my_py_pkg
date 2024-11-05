82e98199b47eebe7bb9c364db44443c7468ba8bf
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-06 05:38:16 +0800

    added reset counter

--- my_py_pkg/number_counter.py
+++ my_py_pkg/number_counter.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from example_interfaces.msg import Int64
+from std_msgs.msg import Int64
+from example_interfaces.srv import SetBool
 
 
-class NumberCounter(Node):
+class NumberCounterNode(Node):
     def __init__(self):
         super().__init__("number_counter")
-        self.subscription = self.create_subscription(
-                Int64, "number", self.number_callback, 10)
-        self.publisher_ = self.create_publisher(Int64, "number_count", 10)
-        self.counter = 0
-        self.get_logger().info("Number Counter has been started")
+        self.number_subscriber = self.create_subscription(
+            Int64, "/number", self.callback_number, 10)
+        self.counter_publisher_ = self.create_publisher(
+            Int64, "/number_count", 10)
+        self.reset_service = self.create_service(
+            SetBool, "/reset_number_count", self.callback_reset_counter)
+        self.get_logger().info("Number Counter Node has been started.")
 
-    def number_callback(self, msg):
+    def callback_number(self, msg):
         self.counter += msg.data
-        count_msg = Int64()
-        count_msg.data = self.counter
-        self.publisher_.publish(count_msg)
-        self.get_logger().info(f"Counter updated: {self.counter}")
+        msg_count = Int64()
+        msg_count.data = self.counter
+        self.counter_publisher.publish(msg_count)
+
+    def callback_reset_counter(self, request, response):
+        if request.data:
+            self.counter = 0
+            response.success = True
+            response.message = "Counter has been reset to 0"
+        else:
+            response.success = False
+            response.message = "Counter was not reset"
+        return response
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = NumberCounter()
+    node = NumberCounterNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
