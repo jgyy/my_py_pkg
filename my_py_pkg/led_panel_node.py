a2cc5974ff21700c9d91c63df0769b07cefd077a
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-11 21:04:34 +0800

    added param values for led

--- my_py_pkg/led_panel_node.py
+++ my_py_pkg/led_panel_node.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from rcl_interfaces.msg import ParameterDescriptor
 from my_robot_interfaces.srv import SetLed
 from my_robot_interfaces.msg import LedStates
 
 
 class LedPanelNode(Node):
     def __init__(self):
-        super().__init__("led_panel_node")
+        super().__init__("led_panel")
+
         self.led_states = [False, False, False]
-        self.service = self.create_service(
-            SetLed,
-            "set_led",
-            self.set_led_callback
-        )
-        self.publisher = self.create_publisher(
-            LedStates,
-            "led_panel_state",
-            10
-        )
+        self.service = self.create_service(SetLed, "set_led", self.set_led_callback)
+        self.publisher = self.create_publisher(LedStates, "led_panel_state", 10)
+        led_states_descriptor = ParameterDescriptor(
+            description="States of LEDs (0 for off, 1 for on)")
+        self.declare_parameter("led_states", [0, 0, 0, 0], led_states_descriptor)
+
+        self_timer_ = self.create_timer(2.0, self.check_led_states)
         self.create_timer(0.1, self.publish_led_states)
-        self.get_logger().info("Battery node initialized")
+        self.get_logger().info("Led Panel node initialized")
+
+    def check_led_states(self):
+        led_states = self.get_parameter("led_states").value
+        state_str = ""
+        for i, state in enumerate(led_states):
+            state_str += f"LED {i}: {'ON' if state == 1 else 'OFF'},"
+        self.get_logger().info(f"Current LED states: {state_str[:-2]}")
 
     def set_led_callback(self, request, response):
         if request.led_number < 1 or request.led_number > 3:
--- my_py_pkg/robot_news_station.py
+++ my_py_pkg/robot_news_station.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from example_interfaces.msg import String
+from std_msgs.msg import String
+from rcl_interfaces.msg import ParameterDescriptor
 
 
 class RobotNewsStationNode(Node):
     def __init__(self):
         super().__init__("robot_news_station")
 
-        self.robot_name_ = "C3P0"
+        robot_name_descriptor = ParameterDescriptor(
+            description="Name of the robot for the news station")
+        self.declare_parameter("robot_name", "R2D2", robot_name_descriptor)
+
         self.publisher_ = self.create_publisher(String, "robot_news", 10)
-        self.timer_ = self.create_timer(0.5, self.publish_news)
+        self.timer_ = self.create_timer(2.0, self.publish_news)
         self.get_logger().info("Robot New Station has been started")
 
     def publish_news(self):
         msg = String()
-        msg.data = "Hi, this is " + str(self.robot_name_) + " from the robot news station."
+        robot_name = self.get_parameter("robot_name").value
+        msg.data = f"Hi, this is {robot_name} from the robot news station."
         self.publisher_.publish(msg)
 
 
