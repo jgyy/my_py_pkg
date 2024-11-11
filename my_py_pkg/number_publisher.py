8dba0f85e5632c62394987a62e1db2b13aa899c7
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-11 20:16:00 +0800

    setting params from py

--- my_py_pkg/number_publisher.py
+++ my_py_pkg/number_publisher.py
@@ -7,17 +7,19 @@ from example_interfaces.msg import Int64
 class NumberPublisher(Node):
     def __init__(self):
         super().__init__("number_publisher")
-        self.declare_parameter("test123")
-        self.declare_parameter("another_param")
+        self.declare_parameter("number_to_publish", 2)
+        self.declare_parameter("publish_frequency", 1.0)
 
+        self.number_ = self.get_parameter("number_to_publish").value
+        self.publish_frequency_ = self.get_parameter("publish_frequency").value
         self.publisher_ = self.create_publisher(Int64, "number", 10)
-        self.timer = self.create_timer(1.0, self.publish_number)
-        self.number = 2
+        self.timer_ = self.create_timer(
+            1.0 / self.publish_frequency_, self.publish_number)
         self.get_logger().info("Number Publisher has been started")
 
     def publish_number(self):
         msg = Int64()
-        msg.data = self.number
+        msg.data = self.number_
         self.publisher_.publish(msg)
 
 
